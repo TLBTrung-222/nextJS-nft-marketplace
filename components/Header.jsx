bf2f1ef2f62c1d62c1c8bdb4a63b34a730d6df75
TrungTLB <baotrung2852003@gmail.com>
2023-10-03 23:22:28 +0700

    set up nextjs app

---
+++ components/Header.jsx
@@ -0,0 +1,23 @@
+import { ConnectButton } from "web3uikit";
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 px-4 text-3xl">NFT Marketplace</h1>
+            <div className="flex flex-row items-center">
+                <Link href="/">
+                    <a className="mr-4 p-6" href="">
+                        Home
+                    </a>
+                </Link>
+                <Link className="mr-4 p-6" href="/sell-nft">
+                    <a className="mr-4 p-6" href="">
+                        Sell page
+                    </a>
+                </Link>
+                <ConnectButton moralisAuth={false} />{" "}
+            </div>
+        </nav>
+    );
+}
--- pages/sell-nft.js
+++ pages/sell-nft.js
@@ -14,7 +14,7 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h1>Homepage</h1>
+            <h1>Sell page</h1>
         </>
     );
 }
